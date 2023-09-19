115a69d143c8138e9b9a20b7e89d6906d3fef08d
md499 <mdhir@bu.edu>
2023-09-19 09:53:59 -0400

    added editext functionality and sqaureroots. Still need to fix issue of solvig sqaure root in function

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,54 +2,148 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.view.View
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import net.objecthunter.exp4j.ExpressionBuilder
-
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var txt: EditText
+
+    private var calc = ""
+    private lateinit var result: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txt = findViewById(R.id.result)
-    }
-    fun onNum(view: View)
-    {
-        txt.append((view as Button).text)
-    }
-    fun onOperator(view: View){
-        txt.append((view as Button).text)
-    }
-    fun onDot(view: View){
-        txt.append((view as Button).text)
-    }
-    fun onDel(view: View){
-        val emptyEditable = Editable.Factory.getInstance().newEditable("")
 
-        // Set the empty Editable as the text of the EditText
-        txt.text = emptyEditable
-    }
-    fun onSqrt(view: View) {
-        txt.append("√")
-    }
 
-    fun onE(view: View) {
-        val text = txt.text.toString()
-        val displayText = text.replace("sqrt", "√")
-        val sanitizedText = text.replace("√", "sqrt") // Replace "√" with "sqrt" for expression evaluation
-        val eval = ExpressionBuilder(sanitizedText).build()
-        val res = eval.evaluate()
+        result = findViewById<EditText>(R.id.result)
 
-        // Check for negative square root results
-        if (res.isNaN()) {
-            txt.text = Editable.Factory.getInstance().newEditable("Error")
-        } else {
-            txt.text = Editable.Factory.getInstance().newEditable(res.toString())
+        val btns = listOf<Button>(
+            findViewById<Button>(R.id.btnZero),
+            findViewById<Button>(R.id.btnOne),
+            findViewById<Button>(R.id.btnTwo),
+            findViewById<Button>(R.id.btnThree),
+            findViewById<Button>(R.id.btnFour),
+            findViewById<Button>(R.id.btnFive),
+            findViewById<Button>(R.id.btnSix),
+            findViewById<Button>(R.id.btnSeven),
+            findViewById<Button>(R.id.btnEight),
+            findViewById<Button>(R.id.btnNine),
+            findViewById<Button>(R.id.btnDot),
+            findViewById<Button>(R.id.btnAdd),
+            findViewById<Button>(R.id.btnMinus),
+            findViewById<Button>(R.id.btnMul),
+            findViewById<Button>(R.id.btnDiv),
+            findViewById<Button>(R.id.btnSqrt)
+        )
+
+        btns.forEach { btn ->
+            btn.setOnClickListener { addBtn(btn.text.toString()) }
         }
+
+        val btnEqual = findViewById<Button>(R.id.btnEqual)
+        val btnClear = findViewById<Button>(R.id.btnClear)
+
+        btnEqual.setOnClickListener { equal(calc) }
+        btnClear.setOnClickListener { clear() }
+
     }
+
+    private fun addBtn(str: String) {
+        calc += str
+        updateRes()
+    }
+
+    private fun clearSqrt(calc: String): String {
+        return calc.replace("sqrt", "s")
+    }
+
+    private fun equal(calc: String) {
+        var calculation = result.text.toString()
+        var currentNumber = StringBuilder()
+        var currentOperator = '+'
+        var total = 0.0
+        var isDecimal = false
+
+        for (char in calculation) {
+            when {
+                char.isDigit() -> {
+                    if (isDecimal) {
+                        currentNumber.append(char)
+                    } else {
+                        currentNumber.append(char)
+                    }
+                }
+
+                char == '.' -> {
+                    isDecimal = true
+                    currentNumber.append(char)
+                }
+
+                char == 's' -> {
+                    if (currentNumber.isNotEmpty()) {
+                        val number = currentNumber.toString().toDouble()
+                        Log.d("num", "$number")
+                        Log.d("char", "$char")
+                        Log.d("total", "$total")
+
+                        // Apply the square root operation as a unary operator
+                        var squareRootResult = sqrt(number)
+
+                        total += squareRootResult
+
+                        // Reset the current operator to none to handle any
+                        //arithmetic after root:NOT WORKING
+
+                        currentOperator = ' '
+
+                        currentNumber = StringBuilder()
+                        isDecimal = false
+                    }
+                }
+
+                char in "+-*/" -> {
+                    if (currentNumber.isNotEmpty()) {
+                        val number = currentNumber.toString().toDouble()
+                        when (currentOperator) {
+                            '+' -> total += number
+                            '-' -> total -= number
+                            '*' -> total *= number
+                            '/' -> total /= number
+                        }
+                        currentNumber = StringBuilder()
+                        currentOperator = char
+                        isDecimal = false
+                    }
+                }
+
+                else -> {}
+            }
+        }
+
+        if (currentNumber.isNotEmpty()) {
+            val number = currentNumber.toString().toDouble()
+            when (currentOperator) {
+                '+' -> total += number
+                '-' -> total -= number
+                '*' -> total *= number
+                '/' -> total /= number
+                else -> total = number
+            }
+        }
+
+        result.setText(total.toString())
+    }
+
+    private fun clear() {
+        calc = ""
+        updateRes()
+    }
+
+    private fun updateRes() {
+        result.setText(calc)
+    }
+
 
 }
